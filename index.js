3d52ee28b5311d048b513f86bb824be7e12bba85
abdulhay103 <abdulhay.me@gmail.com>
2024-02-23 20:56:39 +0600

    delete route done

--- index.js
+++ index.js
@@ -35,7 +35,17 @@ app.get("/books/:id", async (req, res) => {
 app.post("/books", async (req, res) => {
   try {
     const { title, desc } = req.body;
-    res.status(200).json({ message: `Successfully create the book ${title}` });
+    res.status(201).json({ message: `Successfully create the book ${title}` });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+// Delete a specific book
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    res.status(200).json({ message: `Delete specific book with id:${id}` });
   } catch (error) {
     res.json({ message: error.message });
   }
