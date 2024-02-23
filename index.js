2d305c2dd2664d71fb76a719495a4481763dfb3d
abdulhay103 <abdulhay.me@gmail.com>
2024-02-23 18:06:27 +0600

    POST request done

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
 const app = express();
-
 const PORT = "3001";
 
+// Apply middleware
+app.use(express.json()); // This middleware for use request body
+app.use(express.urlencoded({ extended: true })); // This middleware for use formdata
+
+// Run the server
 app.listen(PORT, () => {
   console.log(`server is running at port no http://localhost:${PORT}`);
 });
@@ -12,6 +16,26 @@ app.listen(PORT, () => {
 app.get("/books", async (req, res) => {
   try {
     res.status(200).json({ message: "All books are returned." });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+// Get a specific book
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    res.status(200).json({ message: `Return specific book with id:${id}` });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+// Create a book
+app.post("/books", async (req, res) => {
+  try {
+    const { title, desc } = req.body;
+    res.status(200).json({ message: `Successfully create the book ${title}` });
   } catch (error) {
     res.json({ message: error.message });
   }
